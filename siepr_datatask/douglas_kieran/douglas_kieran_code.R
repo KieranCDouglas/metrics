d29a801dbcc26e8355db5292a75436e69fd97457
KieranCDouglas <kieran@arizona.edu>
2025-10-13 12:36:48 -0700

    messing with positron

--- siepr_datatask/douglas_kieran/douglas_kieran_code.R
+++ siepr_datatask/douglas_kieran/douglas_kieran_code.R
@@ -3,15 +3,15 @@
 ### Script Name: douglas_kieran_code.R
 ### Created By: Kieran Douglas
 ### Last Edited By: Kieran Douglas
-### Date: 10/12/2025
+### Date: 10/13/2025
 ### Description: This is an empirical evaluation of the Small Business Training Program and its effect on firm productivity
 
 ### --- Package installation and loading data --- ###
 install.packages(c("tidyverse", "car", "randomForest", "ivreg", "fixest", "sandwich", "lubridate", "ggthemes", "gtsummary","gt", "modelsummary"))
 lapply(c("tidyverse", "car", "randomForest", "ivreg", "fixest", "sandwich", "lubridate", "ggthemes", "gtsummary", "gt", "modelsummary"), library, character.only = TRUE)
 # Change path names for replication
-firm_data <- read_csv("Documents/MASTERS/METRICS/code/metrics/siepr_datatask/inputs/firm_information.csv")
-ag_sales <- read_csv("Documents/MASTERS/METRICS/code/metrics/siepr_datatask/inputs/aggregate_firm_sales.csv")
+firm_data <- read_csv("/Users/kieran/Documents/MASTERS/METRICS/code/metrics/siepr_datatask/inputs/firm_information.csv")
+ag_sales <- read_csv("/Users/kieran/Documents/MASTERS/METRICS/code/metrics/siepr_datatask/inputs/aggregate_firm_sales.csv")
 # Since the monthly data was provided across many .csv files, we need to merge them to one workable file.
 monthly_files <- list.files(path = "/Users/kieran/Documents/MASTERS/METRICS/code/metrics/siepr_datatask/inputs/monthly_data", pattern = "\\.csv$", full.names = TRUE)
 monthly_data <- map_dfr(monthly_files, read_csv)
